5533930f997ac3e5102f13ab7a4f8a702963b314
gabrielrportugal <gabrielrportugal@outlook.com>
2023-07-11 18:18:37 -0300

    creating application domains

---
+++ src/core/entities/unique-entity-id.ts
@@ -0,0 +1,17 @@
+import { randomUUID } from "crypto";
+
+export class UniqueEntityId {
+  private value: string;
+
+  public toString() {
+    return this.value;
+  }
+
+  public toValue() {
+    return this.value;
+  }
+ 
+  constructor(value?: string) {
+    this.value = value ?? randomUUID();
+  }
+}
