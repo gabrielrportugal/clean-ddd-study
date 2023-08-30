7cfded6673b73158dda7a82e18ed130f6ce2edca
gabrielrportugal <gabrielrportugal@outlook.com>
2023-08-30 11:48:03 -0300

    feat: eslint config and create question use case

---
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -0,0 +1,29 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string
+  questionId: string
+  content: string
+}
+
+export class AnswerQuestionUseCase {
+  constructor(private answersRepository: AnswersRepository) {}
+
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest) {
+    const answer = Answer.create({
+      content,
+      authorId: new UniqueEntityId(instructorId),
+      questionId: new UniqueEntityId(questionId),
+    })
+
+    await this.answersRepository.create(answer)
+
+    return answer
+  }
+}
