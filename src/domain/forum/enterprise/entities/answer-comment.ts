c0e9d92790f1eb8f7bbe3a5dcfd7789ac9ef63fe
gabrielrportugal <gabrielrportugal@outlook.com>
2023-09-26 16:37:21 -0300

    feat: crud use cases finished

--- src/domain/forum/enterprise/entities/answer-comment.ts
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -2,7 +2,7 @@ import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 import { Comment, CommentProps } from '../entities/comment'
 
-interface AnswerCommentProps extends CommentProps {
+export interface AnswerCommentProps extends CommentProps {
   answerId: UniqueEntityId
 }
 
